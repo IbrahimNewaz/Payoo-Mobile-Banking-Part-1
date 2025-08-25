f204ec184485d847010dab6c627a446aaaa1caaa
IbrahimNewaz <ibrahim.codecoffe@gmail.com>
2025-08-26 00:52:01 +0600

    problem found

--- payoo-DOM-Js/payoo_bank.js
+++ payoo-DOM-Js/payoo_bank.js
@@ -2,4 +2,23 @@
 document.getElementById('btn-login')
     .addEventListener('click', function (e) {
         e.preventDefault();
+
+        const mobileNumber = 12345678910;
+        const pinNumber = 7378;
+
+        const mobileNumberValue = document.getElementById('phone-Number').value;
+        const mobileNumberConvert = parseInt(mobileNumberValue);
+
+        const pinNumberValue = document.getElementById('pin-Number').value;
+        const pinNumberConvert = parseInt(pinNumberValue);
+
+        if (mobileNumberConvert === mobileNumber && pinNumberConvert === pinNumber) {
+            console.log('All value is valid');
+        }
+        else {
+            console.log('Invaild input');
+        }
+
+
+
     })
