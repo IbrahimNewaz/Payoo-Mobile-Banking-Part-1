54da9457540f50143fe19de79223187b3ea49c36
IbrahimNewaz <ibrahim.codecoffe@gmail.com>
2025-08-26 07:41:24 +0600

    Balance deposit transaction

---
+++ payoo-DOM-Js/payoo_home.js
@@ -0,0 +1,32 @@
+document.getElementById('btn-addMoney')
+    .addEventListener('click', function (e) {
+        e.preventDefault(); //for stoping form refreshing //
+
+        const PinNumber = 7378;
+
+        // Selecting user input //
+        const pickBank = document.getElementById('select-Bank').value;
+        const acNumber = document.getElementById('ac-Number').value;
+        const addAmount = parseInt(document.getElementById('add-Amount').value);
+        const homePinNumber = parseInt(document.getElementById('home-pinNumber').value);
+
+        // account number checking //
+        if (acNumber.length < 11) {
+            alert("Please Provide Valid Account Number")
+            return;
+        }
+
+        // pin number checking //
+        if (homePinNumber != PinNumber) {
+            alert("Invalid Pin Number")
+            return;
+        }
+
+
+        // Balance deposit transaction //
+        const currentBalance = parseInt(document.getElementById('Balance').innerText);
+        const NewcurentBalance = addAmount + currentBalance;
+        document.getElementById('Balance').innerText = NewcurentBalance;
+
+
+    })
